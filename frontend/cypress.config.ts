fae0c82d67237a90c615775f396c46435005e773
Joao Pedro Renan <jota.renan@gmail.com>
2022-06-28 01:07:14 -0300

    trying to integrate cypress w/ pipeline

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  chromeWebSecurity: false,
+  pageLoadTimeout: 60_000,
+  defaultCommandTimeout: 60_000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
