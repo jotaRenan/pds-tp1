ecea6b4682182c08dda3b76191ff26becf289621
Jean George <jean.george.alves@gmail.com>
2022-06-29 11:48:00 -0300

    add get_team_by_name test

--- backend/BETinho/betinho/application/repositories/tests/test_team_repository_impl.py
+++ backend/BETinho/betinho/application/repositories/tests/test_team_repository_impl.py
@@ -17,7 +17,7 @@ class TestTeamRepositoryImpl(unittest.TestCase):
         self.mock_team_model_module = sys.modules['BETinho.betinho.application.models.team_model']
         self.team_repository = TeamRepositoryImpl()
 
-    def test_save_calls_event_model_save(self):
+    def test_save_calls_team_model_save(self):
         team = Team(uuid.uuid4(), "team")
 
         from_team_return_mock = Mock()
@@ -30,3 +30,14 @@ class TestTeamRepositoryImpl(unittest.TestCase):
         from_team_return_mock.save.assert_called_once()
         self.mock_team_model_module.TeamModel.from_team.assert_called_once_with(
             team)
+
+    def test_get_team_by_name(self):
+        mock_team_model = Mock()
+        expected_team = Team(uuid.uuid4(), "Cruzeiro")
+        mock_team_model.to_team = Mock(return_value=expected_team)
+        self.mock_team_model_module.TeamModel.objects.get = Mock(return_value=mock_team_model)
+        
+        actual = self.team_repository.get_team_by_name("Cruzeiro")
+
+        self.assertEqual(expected_team, actual)
+        mock_team_model.to_team.assert_called_once()
